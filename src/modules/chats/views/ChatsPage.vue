fe48ec1a6a1fda2febbca6d30f67ed7b99cf9440
Nikita <kucenko.nikita@gmail.com>
2023-05-14 20:31:48 +0700

    Add socket connection

--- src/modules/chats/views/ChatsPage.vue
+++ src/modules/chats/views/ChatsPage.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="chats-page">
-
+    <CustomButton @click="sendMessage">
+      Click me
+    </CustomButton>
   </div>
 </template>
 
 <script setup lang="ts">
+import {io} from "socket.io-client";
+import {onDeactivated, onMounted} from "vue";
+import CustomButton from "@/modules/UI-kit/components/CustomButton.vue";
+import {ref} from "@vue/runtime-core";
 
+const socket = io('http://localhost:3000/');
+const messages = ref<any>([]);
+
+const sendMessage = () => {
+  socket.emit('newMessage', 'message');
+}
+
+onMounted(() => {
+  socket.on('connect', () => {
+    console.log('connect is successfully');
+  })
+})
+
+onDeactivated(() => {
+  socket.on('disconnect', () => {
+    console.log('disconnected');
+  })
+})
 </script>
 
 <style scoped>
